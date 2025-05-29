41735d3cdcd27497975481ab9c4bfb231c8c7fe6
Radew-lab-pixel <ktmqliu@gmail.com>
2025-05-29 22:41:19 +1000

    Add Flask RESTX structure

--- app/run.py
+++
@@ -1,8 +0,0 @@
-# main application python3 run.py
-from app import create_app
-from flask import Flask, render_template
-from flask_restx import Api
-from flask_jwt_extended import JWTManager
-from app.api.v1.users import api as users_n
-
-
