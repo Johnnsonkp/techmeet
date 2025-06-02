bec5e440a24f86c8e57ed46e203d0a3a0ce36677
Crystal-holberton <9188@holbertonstudents.com>
2025-06-02 18:31:30 +1000

    Added user model

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -2,11 +2,13 @@
 from flask import Flask
 from flask_restx import Api
 from flask_jwt_extended import JWTManager
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 # from app.api.v1.users import api as users_ns
+from app.api.v1.routes.oauth_connection import api as oauth_ns
+from app.config import config as config_map
 import os
 
-# db = SQLAlchemy()
+db = SQLAlchemy()
 jwt = JWTManager()
 
 def create_app(config_class="config.DevelopmentConfig"):
@@ -14,10 +16,9 @@ def create_app(config_class="config.DevelopmentConfig"):
   """ method used to create an app instance"""
 
   app = Flask(__name__)
+  app.config.from_object(config_map.get(config_class, config_map['default']))
 
   api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
-
-  from app.api.v1.routes.oauth_connection import api as oauth_ns
 
   # Register the namespaces
   # api.add_namespace(users_ns, path='/api/v1/users')
@@ -30,8 +31,11 @@ def create_app(config_class="config.DevelopmentConfig"):
   app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'  # Use a strong and unique key in production
 
    # Initialize the database with the app
-  # db.init_app(app)
+  db.init_app(app)
   jwt.init_app(app)
   # jwt = JWTManager(app)
+
+  with app.app_context():
+    db.create_all()
 
   return app
