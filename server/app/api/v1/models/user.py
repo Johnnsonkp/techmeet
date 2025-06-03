ae574cb277d85a9d5d37c6482e823e852a9dbf39
chak8x8 <chak8x8@gmail.com>
2025-06-03 15:27:55 +1000

    User.py

---
+++ server/app/api/v1/models/user.py
@@ -0,0 +1,41 @@
+"""User Model"""
+
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=True)
+    last_name = db.Column(db.String(50), nullable=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    address = db.Column(db.String(200), nullable=True)
+    password = db.Column(db.String(128), nullable=True)
+    is_admin = db.Column(db.Boolean, default=False, nullable=False)
+    employment_status = db.Column(db.String(50), nullable=True)
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+    oauth_connections = db.relationship('OauthConnection', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+    def register_user(data):
+        hashed_password = bcrypt.generate_password_hash(data.get('password')).decode('utf-8')
+        user = User(
+            email = data.get('email'),
+            first_name = data.get('first_name'),
+            last_name = data.get('last_name'),
+            address = data.get('address'),
+            password = hashed_password,
+            is_admin = data.get('is_admin'),
+            employment_status = data.get('employment_status')
+        )
+        db.session.add(user)
+        db.session.commit()
+        return user
