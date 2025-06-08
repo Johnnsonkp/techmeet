0cba1d65b6d4a20b170fced2eca910c628dbb79b
John Nkp <john.nkp1@gmail.com>
2025-06-09 01:16:29 +1000

    database schema for the following tables created: users, profiles, oauthconnections

--- server/app/config.py
+++ server/app/config.py
@@ -1,44 +1,8 @@
-# import os
-# from dotenv import load_dotenv
-
-# load_dotenv()  # for loading .env configuration
-
-# class Config:
-#     SECRET_KEY = os.getenv('SECRET_KEY')
-#     SQLALCHEMY_DATABASE_URI = os.environ.get(
-#         'DATABASE_URL',
-#         'mysql+pymysql://root:@localhost:3306/techmeet'
-#     )
-#     # SQLALCHEMY_DATABASE=f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}\
-#     #   @{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
-    
-# class DevelopmentConfig(Config):
-#     DEBUG = True
-#     # SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
-#     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://hbnb:hbnb_password@localhost/hbnb_db'
-#     # Database Config (Default to MySQL via PyMySQL)
-#     # SQLALCHEMY_DATABASE_URI = os.environ.get(
-#     #     'DATABASE_URL',
-#     #     'mysql+pymysql://root:password@localhost:3306/techmeet'
-#     # )
-
-# class ProductionConfig(Config):
-#     DEBUG = False
-#     # required explicit settung in production
-#     if not os.getenv('SECRET_KEY'):
-#         raise RuntimeError("SECRET_KEY Must set in production")
-
-# config = {
-#     'development': DevelopmentConfig,
-#     'production': ProductionConfig,
-#     'default': DevelopmentConfig
-# }
 import os
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
-
 
 class BaseConfig:
     """Base configuration."""
