dd16f67044430c44f0392e7becf533680dad98ca
Crystal-holberton <9918@holbertonstudents.com>
2025-06-11 14:34:11 +1000

    craeted auth UI

---
+++ client/app/auth/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+import AuthCard from "@/components/auth/AuthCard";
+
+export default function AuthPage() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <AuthCard />
+    </div>
+  );
+}
