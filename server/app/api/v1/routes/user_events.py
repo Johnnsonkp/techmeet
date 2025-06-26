b772782aa5814a1267d40be419e913a4acff3735
Radew-lab-pixel <ktmqliu@gmail.com>
2025-06-26 14:45:21 +1000

    added GET /api/v1/user_events/ and code cleanup

--- server/app/api/v1/routes/user_events.py
+++ server/app/api/v1/routes/user_events.py
@@ -14,7 +14,36 @@ user_event_model = api.model('UserEvent', {
     'event_id': fields.Integer(required=True, description='Event ID')
 })
 
-# get the events of specific user
+# get the events of currently logged in user
+@api.route("/")
+class UserEvents(Resource):
+    @jwt_required()
+    def get(self):
+        """Get all the events registered to currently logged in user"""
+        current_user_id = get_jwt_identity()
+
+        user = User.query.filter_by(user_id=current_user_id).first()
+        if not user:
+            return {"error": "User not found"}, 404
+        events = [{
+            'event_id': event.id,
+            'name': event.name,
+            'description': event.description,
+            'price': event.price,
+            'location' : event.location,
+            'date' : event.date,
+            'time' : event.time.isoformat(),
+        } for event in user.events]  # using the user.events relationship
+        
+        response = {
+            "user_id": user.user_id,
+            "first_name": user.first_name,
+            "last name": user.last_name,
+            "events": events
+        }
+        return response, 200
+
+# get the events of specific user ( only admin )
 @api.route('/<int:user_id>')
 class UserEventsList(Resource):
     # @api.doc(security='Bearer Auth')
@@ -22,7 +51,7 @@ class UserEventsList(Resource):
     @api.marshal_with(user_event_model)
     @jwt_required()
     def get(self, user_id):
-        """Get all events for a specific user"""
+        """Get all events for a specific user ( only admin )"""
         current_user_id = get_jwt_identity()
         
         # Authorization check
@@ -50,30 +79,30 @@ class UserEventsList(Resource):
         #             [event_list.name for event_list in events_list]}),201
 
         events = [{
-            'Event ID': event.id,
-            'Name': event.name,
-            'Description': event.description,
-            'Price': event.price,
-            'Location' : event.location,
-            'Date' : event.date,
-            'Time' : event.time.isoformat(),
+            'event_id': event.id,
+            'name': event.name,
+            'description': event.description,
+            'price': event.price,
+            'location' : event.location,
+            'date' : event.date,
+            'time' : event.time.isoformat(),
         } for event in user.events]  # using the user.events relationship
         
         response = {
-            "User ID": user.user_id,
-            "First name": user.first_name,
-            "Last name": user.last_name,
-            "Events": events
+            "user_id": user.user_id,
+            "first_name": user.first_name,
+            "last name": user.last_name,
+            "events": events
         }
         
         return response, 200
 
-# get registered users of the specific event    
+# get registered users of the specific event ( admin only )   
 @api.route('/<int:event_id>')
 #@api.route('/events/<int:event_id>/users')
 class EventUsersList(Resource):
     def get(self, event_id):
-        """Get all registered users for a specific event"""
+        """Get all registered users for a specific event ( only admin )"""
         # might need to implement admin user check
         # ...
         # check if the event_id is in the database
@@ -92,19 +121,19 @@ class EventUsersList(Resource):
         
         # Prepare the response data
         response = {
-            "Event ID": event.event_id,
-            "Event Name": event.name,
-            "Description": event.description,
-            "Price": event.price,
-            "Location" : event.location,
-            "Date" : event.date,
-            "Time" : event.time.isoformat(),
+            "event_id": event.event_id,
+            "name": event.name,
+            "description": event.description,
+            "price": event.price,
+            "location" : event.location,
+            "date" : event.date,
+            "time" : event.time.isoformat(),
             "registered_users": [
                 {
-                    "User ID": attendee.user.user_id,
-                    "First Name": attendee.user.first_name,
-                    "Last Name": attendee.user.last_name,
-                    "Employment Status": attendee.user.employment_status,
+                    "user_id": attendee.user.user_id,
+                    "first_name": attendee.user.first_name,
+                    "last_name": attendee.user.last_name,
+                    "employment_status": attendee.user.employment_status,
                 }
                 for attendee in attendees
                 if attendee.user  # Ensure the user exists
@@ -112,13 +141,83 @@ class EventUsersList(Resource):
         }
         
         return response, 200
+    
+    @jwt_required()
+    def post(self, event_id): 
+        """Post/ Add an event to current logged in user"""
+        current_user_id = get_jwt_identity()
+                        
+        user = User.query.filter_by(user_id=current_user_id).first()
+        if not user:
+            return {"error": "User not found"}, 404
+        
+        # check if the event_id is in the database
+        event = Event.query.filter_by(event_id=event_id).first()
+        if not event:
+             return {"error": f"Event {event_id} not found"}, 404
+       
+
+        # Check if attendance already exists
+        if UserEvent.query.filter_by(user_id=current_user_id, event_id=event_id).first():
+            return {"error": f"User {current_user_id} already attending event {event_id}"}, 409
+
+        attendee = UserEvent(user_id=current_user_id, event_id=event_id)
+        db.session.add(attendee)
+        db.session.commit()
+
+        return {
+            "message": "Added successfully",
+            "user_id": attendee.user_id,
+            "event_id": attendee.event_id,
+            "description": event.description,
+            "price": event.price,
+            "location" : event.location,
+            "date" : event.date,
+            "time" : event.time.isoformat(),
+
+        }, 201
+        
+    @jwt_required()
+    def delete(self, event_id):
+        """Delete an event attached to current logged in user"""
+        current_user_id = get_jwt_identity()
+                        
+        user = User.query.filter_by(user_id=current_user_id).first()
+        if not user:
+            return {"error": "User not found"}, 404
+        
+        # check if the event_id is in the database
+        event = Event.query.filter_by(event_id=event_id).first()
+        if not event:
+             return {"error": f"Event {event_id} not found"}, 404
+        
+        attendee = UserEvent.query.filter_by(user_id=current_user_id, event_id=event_id).first()
+
+        # search if the event exists and assigned to the current user
+        if not attendee:
+            return {"error": f"Event {event_id} not found thus can't be deleted, please check the event detail"}, 409
+
+        db.session.delete(attendee)
+        db.session.commit()
+
+        return ({   "message" : "Deleted successfully",
+                    "event_id" : event_id,
+                    "name" : event.name,
+                    "description" : event.description,
+                    "price": event.price,
+                    "location" : event.location,
+                    "date" : event.date,
+                    "time" : event.time.isoformat(),
+        }), 201
+
 
     
 @api.route('/<int:user_id>/<int:event_id>')
 class UserEvents(Resource):
+    # Admin accesss only
     # "Add an event to user_events table that a specific user attending"
     def post(self, user_id, event_id):
-        """Register user to a specific event"""
+        """Register user to a specific event ( only admin )"""
         current_user_id = get_jwt_identity()
 
         # Authorization check as only the login current user can attached his/her own event
@@ -157,7 +256,7 @@ class UserEvents(Resource):
         }, 201
 
     def delete(self, user_id, event_id):
-        """ Delete a specific event which registered to a specific user """
+        """ Delete a specific event which registered to a specific user ( only admin )"""
         
         current_user_id = get_jwt_identity()
         # Authorization check as only the login current user can attached his/her own event
