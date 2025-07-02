df432143c20d694189ffa0728597046afe1d1a3e
John Nkp <john.nkp1@gmail.com>
2025-07-03 06:57:30 +1000

    hotfix

--- server/run_deploy.sh
+++ server/run_deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 echo "Environment: $FLASK_ENV"
 
@@ -6,5 +7,11 @@ echo "Environment: $FLASK_ENV"
 echo "Running DB migrations..."
 flask db upgrade
 
-echo "Starting Gunicorn server on port ${PORT:-8080}..."
-gunicorn run:app --bind 0.0.0.0:${PORT:-8080}
+if [ "$FLASK_ENV" != "production" ]; then
+  echo "Starting Flask development server on port 5328..."
+  export FLASK_APP=run.py
+  flask run --host='localhost' --port=5328
+else
+  echo "Starting Gunicorn server on port ${PORT:-8080}..."
+  gunicorn run:app --bind 0.0.0.0:${PORT:-8080}
+fi
