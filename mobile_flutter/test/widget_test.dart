f1b42c454b6f1af2ebd0750512f33dccbd6e6cb6
chak8x8 <chak8x8@gmail.com>
2025-07-22 11:15:29 +1000

    Update pubspec, widget_test and Info

--- mobile_flutter/test/widget_test.dart
+++ mobile_flutter/test/widget_test.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:techmeet_mobile/main.dart';
+import 'package:mobile_flutter/main.dart';
 
 void main() {
   testWidgets('TechMeetApp renders correctly', (WidgetTester tester) async {
-    // Build the app and trigger a frame.
-    await tester.pumpWidget(TechMeetApp());
-
-    // Verify that the Login screen is displayed initially (since _token is null).
+    await tester.pumpWidget(const TechMeetApp());
     expect(find.text('Login'), findsOneWidget);
   });
 }
